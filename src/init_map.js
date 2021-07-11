a91a1a2a2cb70b9dcdb23ad99ac96e6efd680fe9
noboomta <puvana.jp36@gmail.com>
2021-07-12 01:48:45 +0700

    add static web

--- src/init_map.js
+++ src/init_map.js
@@ -1,19 +1,20 @@
-// import "https://api.longdo.com/map/?key=a18ea1e3fa5aacd889ae1028d3709545"
-
-// const fs = require('fs')
-
 function checkDistance(displacementResult){
 	console.log(displacementResult + " m.")
 	return (displacementResult/1000).toFixed(2)
 }
-
+var myMap
+var myDragPin
 function init() {
-	map = new longdo.Map({
+	var map = new longdo.Map({
 		placeholder: document.getElementById('map'),
 	})
+	map.bound({
+		minLon: 100.635, minLat: 13.989,
+		maxLon: 100.675, maxLat: 13.99
+	  });
 	map.location(longdo.LocationMode.Geolocation);
 	map.Layers.setBase(longdo.Layers.GRAY);
-	map.Layers.add(longdo.Layers.TRAFFIC);
+	// map.Layers.add(longdo.Layers.TRAFFIC);
 
 	var places = []
 	places.push({ "title": 'Clubhouse', "detail": 'clubhouse', "location": {lat: 13.987699, lon: 100.657391} })
@@ -31,4 +32,56 @@ function init() {
 			detail: place['detail']
 		})
 	);
+
+	const fs = require('fs')
+	fs.readFile('./place.json', 'utf8', (err, jsonString) => {
+		console.log(jsonString)
+	})
+
+	myMap = map
+}
+
+function addDrag() {
+	myMap.location(longdo.LocationMode.Geolocation);
+	console.log(myMap.location())
+	var dragPin = new longdo.Marker(myMap.location(),
+		{
+		  title: 'Marker',
+		  icon: {
+			url: 'https://map.longdo.com/mmmap/images/pin_mark.png',
+			offset: { x: 12, y: 45 }
+		  },
+		  detail: 'Drag me',
+		  draggable: true,
+		  weight: longdo.OverlayWeight.Top,
+		});
+	myMap.Overlays.add(dragPin)
+	myDragPin = dragPin
+	var result = myDragPin.location()
+	document.getElementById("lat").value = result.lat
+	document.getElementById("lon").value = result.lon
+}
+
+function logDragPin() {
+	if(myDragPin){
+		var result = myDragPin.location()
+		document.getElementById("lat").value = result.lat
+		document.getElementById("lon").value = result.lon
+		console.log(myDragPin.location())
+	}
+}
+
+function get_drag_pin_location() {
+	return myDragPin.location()
+}
+
+function getNewHouse() {
+	var address = document.getElementById("address").value
+	var linename = document.getElementById("linename").value
+	var status = document.getElementById("status").value
+	var lat = document.getElementById("lat").value
+	var lon = document.getElementById("lon").value
+	console.log(address);
+
+	alert("Added")
 }
---
+++ src/submit_form.js
@@ -0,0 +1,8 @@
+var address = document.getElementById("address").value
+var linename = document.getElementById("linename").value
+var status = document.getElementById("status").value
+var lat = document.getElementById("lat").value
+var lon = document.getElementById("lon").value
+console.log(address);
+
+alert("Added")
